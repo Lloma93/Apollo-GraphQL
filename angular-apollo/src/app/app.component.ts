6705c6e0de098fbf88232dd65c5eb2f177a0d638
lloma93 <palloma.adolfi@gmail.com>
2019-02-04 16:35:03 -0200

    apollo query

--- angular-apollo/src/app/app.component.ts
+++ angular-apollo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-apollo';
+  private apiUrl = '';
+
+  constructor(
+    private apollo: Apollo
+  ) {
+    this.allUsers();
+  }
+
+  allUsers(): void {
+    this.apollo.query({
+      query: gql`
+      query {
+        allUsers{
+          id
+          name
+          email
+        }
+      }
+    `
+    }).subscribe(res => console.log('Query: ', res));
+  }
+
+  createUser(): void {
+    this.apollo.query({
+      query: gql`
+        mutation CreateNewUser($)
+      `
+    })
+  }
 }
